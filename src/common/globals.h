38f0cf42783808902470e57f1c7bc819b805578e
Seyyit Genc <seyyitgenc08@gmail.com>
2023-12-26 15:01:56 +0300

    added linux support

--- src/common/globals.h
+++ src/common/globals.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#ifdef _WIN64
 #include <winsock2.h>
 #include <ws2tcpip.h>
+
+#elif defined(__linux__)
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
 #include <iostream>
 
 #define DEFAULT_ADDR_FAMILY     AF_INET
